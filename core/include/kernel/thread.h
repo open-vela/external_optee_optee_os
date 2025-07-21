OP-TEE/optee_os

Basic fault mitigation routines
Adds basic fault mitigation routines designed to help protecting from
fault injection attacks on the hardware. This is by no means bullet
proof, but it should at least improve the situation.

These routines focus on verifying that a function has been called and
that the returned value matches the result from the function. This is
done by having a handshake between the caller and the callee where also
the return value is transmitted in a separate channel.

Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>

core/include/kernel/thread.h
@@ -43,6 +43,9 @@ struct thread_specific_data {
 	bool stackcheck_recursion;
 #endif
 	unsigned int syscall_recursion;
+#ifdef CFG_CORE_FAULT_MITIGATION
+	struct ftmn_func_arg *ftmn_arg;
+#endif
 };
 
 void thread_init_canaries(void);

mk/config.mk
@@ -885,3 +885,7 @@ CFG_DRIVERS_TPM2_MMIO ?= n
 ifeq ($(CFG_CORE_TPM_EVENT_LOG),y)
 CFG_CORE_TCG_PROVIDER ?= $(CFG_DRIVERS_TPM2)
 endif
+
+# Enables best effort mitigations against fault injected when the hardware
+# is tampered with. Details in lib/libutils/ext/include/fault_mitigation.h
+CFG_CORE_FAULT_MITIGATION ?= y

open-vela/external_optee_optee_os

Acked-by: Jerome Forissier <jerome.forissier@linaro.org>

core/include/kernel/thread.h
@@ -42,6 +42,9 @@ struct thread_specific_data {
 	bool stackcheck_recursion;
 #endif
 	unsigned int syscall_recursion;
+#ifdef CFG_FAULT_MITIGATION
+	struct ftmn_func_arg *ftmn_arg;
+#endif
 };
 
 void thread_init_canaries(void);

mk/config.mk
@@ -953,3 +953,7 @@ endif
 # Developer can open a stats PTA session and then invoke command
 # STATS_CMD_TA_STATS to get the context of loaded TAs.
 CFG_TA_STATS ?= n
+
+# Enables best effort mitigations against fault injected when the hardware
+# is tampered with. Details in lib/libutils/ext/include/fault_mitigation.h
+CFG_FAULT_MITIGATION ?= y