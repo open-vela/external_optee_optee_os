OP-TEE/optee_os

ta: pkcs11: add id-to-string conversion for new TA commands
Add missing user authentication commands id-to-string conversion.
Add new import/destroy commands id-to-string conversion.

Signed-off-by: Etienne Carriere <etienne.carriere@linaro.org>

ta/pkcs11/src/pkcs11_helpers.c
@@ -71,6 +71,13 @@ static const struct any_id __maybe_unused string_ta_cmd[] = {
 	PKCS11_ID(PKCS11_CMD_SESSION_INFO),
 	PKCS11_ID(PKCS11_CMD_CLOSE_SESSION),
 	PKCS11_ID(PKCS11_CMD_CLOSE_ALL_SESSIONS),
+	PKCS11_ID(PKCS11_CMD_INIT_TOKEN),
+	PKCS11_ID(PKCS11_CMD_INIT_PIN),
+	PKCS11_ID(PKCS11_CMD_SET_PIN),
+	PKCS11_ID(PKCS11_CMD_LOGIN),
+	PKCS11_ID(PKCS11_CMD_LOGOUT),
+	PKCS11_ID(PKCS11_CMD_IMPORT_OBJECT),
+	PKCS11_ID(PKCS11_CMD_DESTROY_OBJECT),
 };
 
 static const struct any_id __maybe_unused string_slot_flags[] = {

open-vela/external_optee_optee_os

Reviewed-by: Ricardo Salveti <ricardo@foundries.io>
Reviewed-by: Rouven Czerwinski <r.czerwinski@pengutronix.de>

ta/pkcs11/src/pkcs11_helpers.c
@@ -71,6 +71,13 @@ static const struct any_id __maybe_unused string_ta_cmd[] = {
 	PKCS11_ID(PKCS11_CMD_SESSION_INFO),
 	PKCS11_ID(PKCS11_CMD_CLOSE_SESSION),
 	PKCS11_ID(PKCS11_CMD_CLOSE_ALL_SESSIONS),
+	PKCS11_ID(PKCS11_CMD_INIT_TOKEN),
+	PKCS11_ID(PKCS11_CMD_INIT_PIN),
+	PKCS11_ID(PKCS11_CMD_SET_PIN),
+	PKCS11_ID(PKCS11_CMD_LOGIN),
+	PKCS11_ID(PKCS11_CMD_LOGOUT),
+	PKCS11_ID(PKCS11_CMD_CREATE_OBJECT),
+	PKCS11_ID(PKCS11_CMD_DESTROY_OBJECT),
 };
 
 static const struct any_id __maybe_unused string_slot_flags[] = {