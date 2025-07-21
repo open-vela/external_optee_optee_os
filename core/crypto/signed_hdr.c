OP-TEE/optee_os

core: support loading TAs signed with a subkey
Adds support to load TAs signed with subkey or a chain of subkeys. This
allows delegation of TA signing without distributing the root key. TAs
signed with a subkey are confined to the UUID-V5 namespace of the subkey
to avoid TA UUID clashes with different subkeys.

SHDR_SUBKEY is a type of header which enables chains of public keys.
The public root key is used to verify the first public subkey, which
then is used to verify the next public subkey and so on.

The TA is finally verified using the last subkey. All these headers are
added in front of the TA binary so everything needed to verify the TA is
available when it's loaded into memory.

For example:
Subkey
  magic:      0x4f545348
  img_type:   3
  img_size:   320
  algo:       0x70414930
  hash_size:  32
  sig_size:   256
  hash:       b'f573f329fe77be686ce71647'...
  uuid:       f04fa996-148a-453c-b037-1dcfbad120a6
  name_size:  64
  subkey_version: 1
  max_depth:  4
  algo:       0x70414930
  attr_count: 2
  next name:  "mid_level_subkey"
Next header at offset: 692 (0x2b4)
Subkey
  magic:      0x4f545348
  img_type:   3
  img_size:   320
  algo:       0x70414930
  hash_size:  32
  sig_size:   256
  hash:       b'f5429adeca4794bbdcc1a9c9'...
  uuid:       d28416d1-fc99-5762-ac9d-d2311e0b0472
  name_size:  64
  subkey_version: 1
  max_depth:  3
  algo:       0x70414930
  attr_count: 2
  next name:  "subkey1_ta"
Next header at offset: 1384 (0x568)
Bootstrap TA
  magic:      0x4f545348
  img_type:   1
  img_size:   137824
  algo:       0x70414930
  hash_size:  32
  sig_size:   256
  hash:       b'6ad3a1d386cb47600c95ec8d'...
  uuid:       dcdfecce-f5a0-554a-b96b-74a3e507fcff
  ta_version: 0
  TA offset:  1712 (0x6b0)
  TA size:    137824 (0x21a60)

TODO check subkey_version

Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>

core/crypto/signed_hdr.c
@@ -1,15 +1,18 @@
 // SPDX-License-Identifier: BSD-2-Clause
 /*
- * Copyright (c) 2015-2017, Linaro Limited
+ * Copyright (c) 2015-2022, Linaro Limited
  */
 
 #include <crypto/crypto.h>
+#include <kernel/panic.h>
+#include <mempool.h>
 #include <signed_hdr.h>
 #include <stdlib.h>
 #include <string.h>
 #include <ta_pub_key.h>
 #include <tee_api_types.h>
 #include <tee/tee_cryp_utl.h>
+#include <tee/uuid.h>
 #include <utee_defines.h>
 #include <util.h>
 
@@ -85,5 +88,265 @@ out:
 	crypto_acipher_free_rsa_public_key(&key);
 	if (res)
 		return TEE_ERROR_SECURITY;
+	return TEE_SUCCESS;
+}
+
+static const struct shdr_subkey_attr *
+find_attr(const struct shdr_subkey *subkey, uint32_t id)
+{
+	size_t n = 0;
+
+	for (n = 0; n < subkey->attr_count; n++)
+		if (subkey->attrs[n].id == id)
+			return subkey->attrs + n;
+
+	return NULL;
+}
+
+static TEE_Result load_rsa_key(const struct shdr_subkey *subkey,
+			       struct rsa_public_key **key_pp)
+{
+	const uint8_t *base = (const uint8_t *)subkey;
+	const struct shdr_subkey_attr *pub_exp = NULL;
+	const struct shdr_subkey_attr *modulus = NULL;
+	struct rsa_public_key *key = NULL;
+	TEE_Result res = TEE_SUCCESS;
+
+	pub_exp = find_attr(subkey, TEE_ATTR_RSA_PUBLIC_EXPONENT);
+	if (!pub_exp)
+		return TEE_ERROR_SECURITY;
+	modulus = find_attr(subkey, TEE_ATTR_RSA_MODULUS);
+	if (!modulus)
+		return TEE_ERROR_SECURITY;
+
+	key = calloc(1, sizeof(*key));
+	if (!key)
+		return TEE_ERROR_SECURITY;
+	res = crypto_acipher_alloc_rsa_public_key(key, modulus->size * 8);
+	if (res)
+		goto err_key;
+
+	res = crypto_bignum_bin2bn(base + pub_exp->offs, pub_exp->size, key->e);
+	if (res)
+		goto err;
+	res = crypto_bignum_bin2bn(base + modulus->offs, modulus->size, key->n);
+	if (res)
+		goto err;
+
+	*key_pp = key;
+	return TEE_SUCCESS;
+err:
+	crypto_acipher_free_rsa_public_key(key);
+err_key:
+	free(key);
+	return TEE_ERROR_SECURITY;
+}
+
+static TEE_Result check_attrs(const struct shdr_subkey *subkey, size_t img_size)
+{
+	const struct shdr_subkey_attr *attrs = subkey->attrs;
+	size_t end = 0;
+	size_t n = 0;
+
+	if (MUL_OVERFLOW(subkey->attr_count, sizeof(*attrs), &end) ||
+	    ADD_OVERFLOW(end, sizeof(*subkey), &end) ||
+	    end > img_size)
+		return TEE_ERROR_SECURITY;
+
+	for (n = 0; n < subkey->attr_count; n++)
+		if (ADD_OVERFLOW(attrs[n].offs, attrs[n].size, &end) ||
+		    end > img_size)
+			return TEE_ERROR_SECURITY;
+
+	return TEE_SUCCESS;
+}
+
+static TEE_Result calc_next_uuid(uint8_t uuid[sizeof(TEE_UUID)],
+				 const uint8_t my_uuid[sizeof(TEE_UUID)],
+				 const void *ns_name, size_t name_size)
+{
+	TEE_Result res = TEE_ERROR_SECURITY;
+	void *ctx = NULL;
+	struct {
+		uint8_t digest[TEE_SHA1_HASH_SIZE];
+		TEE_UUID uuid;
+		char name_str[];
+	} *tmp;
+
+	if (!name_size) {
+		memcpy(uuid, my_uuid, sizeof(TEE_UUID));
+		return TEE_SUCCESS;
+	}
+
+	/* RFC 4122 requires SHA-1 for UUID v5 */
+	if (crypto_hash_alloc_ctx(&ctx, TEE_ALG_SHA1))
+		return TEE_ERROR_SECURITY;
+
+	tmp = mempool_alloc(mempool_default, sizeof(*tmp) + name_size);
+	if (!tmp)
+		goto out_ctx;
+	memcpy(tmp->name_str, ns_name, name_size);
+
+	if (crypto_hash_init(ctx) ||
+	    crypto_hash_update(ctx, my_uuid, sizeof(TEE_UUID)) ||
+	    crypto_hash_update(ctx, (const void *)tmp->name_str,
+			       strnlen(tmp->name_str, name_size)) ||
+	    crypto_hash_final(ctx, tmp->digest, sizeof(tmp->digest)))
+		goto out_mempool;
+
+	tee_uuid_from_octets(&tmp->uuid, tmp->digest);
+	/*
+	 * Set the four most significant bits (bits 12 through 15) of the
+	 * time_hi_and_version field to 5.
+	 */
+	tmp->uuid.timeHiAndVersion &= ~SHIFT_U32(0xf, 12);
+	tmp->uuid.timeHiAndVersion |= SHIFT_U32(5, 12);
+	/*
+	 * Set the two most significant bits (bits 6 and 7) of the
+	 * clock_seq_hi_and_reserved to zero and one, respectively.
+	 */
+	tmp->uuid.clockSeqAndNode[0] &= ~BIT(6);
+	tmp->uuid.clockSeqAndNode[0] |= BIT(7);
+
+	tee_uuid_to_octets(uuid, &tmp->uuid);
+	res = TEE_SUCCESS;
+
+out_mempool:
+	mempool_free(mempool_default, tmp);
+out_ctx:
+	crypto_hash_free_ctx(ctx);
+
+	return res;
+}
+
+TEE_Result shdr_load_pub_key(const struct shdr *shdr, size_t offs,
+			     const uint8_t *ns_img, size_t ns_img_size,
+			     const uint8_t next_uuid[sizeof(TEE_UUID)],
+			     uint32_t max_depth, struct shdr_pub_key *key)
+{
+	struct shdr_subkey *subkey = NULL;
+	TEE_Result res = TEE_SUCCESS;
+	void *digest = NULL;
+	uint8_t *img = NULL;
+	void *ctx = NULL;
+	size_t end = 0;
+
+	if (shdr->img_type != SHDR_SUBKEY)
+		return TEE_ERROR_SECURITY;
+
+	if (shdr->img_size < sizeof(*subkey))
+		return TEE_ERROR_SECURITY;
+
+	if (ADD_OVERFLOW(shdr->img_size, offs, &end) || end > ns_img_size)
+		return TEE_ERROR_SECURITY;
+
+	img = mempool_alloc(mempool_default, shdr->img_size + shdr->hash_size);
+	if (!img)
+		return TEE_ERROR_OUT_OF_MEMORY;
+	memcpy(img + shdr->hash_size, ns_img + offs, shdr->img_size);
+	subkey = (void *)(img + shdr->hash_size);
+	digest = img;
+
+	if (crypto_hash_alloc_ctx(&ctx, TEE_DIGEST_HASH_TO_ALGO(shdr->algo))) {
+		res = TEE_ERROR_SECURITY;
+		goto out_mempool;
+	}
+
+	if (crypto_hash_init(ctx) ||
+	    crypto_hash_update(ctx, (const void *)shdr, sizeof(*shdr)) ||
+	    crypto_hash_update(ctx, (const void *)subkey, shdr->img_size) ||
+	    crypto_hash_final(ctx, digest, shdr->hash_size) ||
+	    memcmp(digest, SHDR_GET_HASH(shdr), shdr->hash_size)) {
+		res = TEE_ERROR_SECURITY;
+		goto out_ctx;
+	}
+
+	res = check_attrs(subkey, shdr->img_size);
+	if (res)
+		goto out_ctx;
+
+	if (subkey->max_depth >= max_depth) {
+		res = TEE_ERROR_SECURITY;
+		goto out_ctx;
+	}
+	if (next_uuid && memcmp(next_uuid, subkey->uuid, sizeof(TEE_UUID))) {
+		res = TEE_ERROR_SECURITY;
+		goto out_ctx;
+	}
+
+	key->max_depth = subkey->max_depth;
+	key->name_size = subkey->name_size;
+	memcpy(key->uuid, subkey->uuid, sizeof(TEE_UUID));
+	if (ADD_OVERFLOW(key->name_size, offs + shdr->img_size, &end) ||
+	    end > ns_img_size) {
+		res = TEE_ERROR_SECURITY;
+		goto out_ctx;
+	}
+	res = calc_next_uuid(key->next_uuid, key->uuid,
+			     ns_img + offs + shdr->img_size, key->name_size);
+	if (res)
+		goto out_ctx;
+
+	key->main_algo = TEE_ALG_GET_MAIN_ALG(subkey->algo);
+	switch (key->main_algo) {
+	case TEE_MAIN_ALGO_RSA:
+		res = load_rsa_key(subkey, &key->pub_key.rsa);
+		break;
+	default:
+		panic();
+	}
+
+out_ctx:
+	crypto_hash_free_ctx(ctx);
+out_mempool:
+	mempool_free(mempool_default, img);
+	return res;
+}
+
+void shdr_free_pub_key(struct shdr_pub_key *key)
+{
+	if (key) {
+		switch (key->main_algo) {
+		case TEE_MAIN_ALGO_RSA:
+			crypto_acipher_free_rsa_public_key(key->pub_key.rsa);
+			free(key->pub_key.rsa);
+			break;
+		default:
+			panic();
+		}
+	}
+}
+
+TEE_Result shdr_verify_signature2(struct shdr_pub_key *key,
+				  const struct shdr *shdr)
+{
+	size_t hash_size = 0;
+
+	if (shdr->magic != SHDR_MAGIC)
+		return TEE_ERROR_SECURITY;
+
+	if (TEE_ALG_GET_MAIN_ALG(shdr->algo) != key->main_algo)
+		return TEE_ERROR_SECURITY;
+
+	if (tee_alg_get_digest_size(TEE_DIGEST_HASH_TO_ALGO(shdr->algo),
+				    &hash_size))
+		return TEE_ERROR_SECURITY;
+	if (hash_size != shdr->hash_size)
+		return TEE_ERROR_SECURITY;
+
+	switch (key->main_algo) {
+	case TEE_MAIN_ALGO_RSA:
+		if (crypto_acipher_rsassa_verify(shdr->algo, key->pub_key.rsa,
+						 shdr->hash_size,
+						 SHDR_GET_HASH(shdr),
+						 shdr->hash_size,
+						 SHDR_GET_SIG(shdr),
+						 shdr->sig_size))
+			return TEE_ERROR_SECURITY;
+		break;
+	default:
+		panic();
+	}
+
 	return TEE_SUCCESS;
 }

ta/arch/arm/link_shlib.mk
@@ -48,7 +48,7 @@ $(link-out-dir)/$(shlibuuid).elf: $(link-out-dir)/$(shlibname).so
 	$(q)ln -sf $(<F) $@
 
 $(link-out-dir)/$(shlibuuid).ta: $(link-out-dir)/$(shlibname).stripped.so \
-				$(TA_SIGN_KEY)
+				$(TA_SIGN_KEY) $(TA_SUBKEY_ARGS)
 	@$(cmd-echo-silent) '  SIGN    $@'
-	$(q)$(PYTHON3) $(SIGN) --key $(TA_SIGN_KEY) --uuid $(shlibuuid) \
-		--in $< --out $@
+	$(q)$(PYTHON3) $(SIGN) --key $(TA_SIGN_KEY) $(TA_SUBKEY_ARGS) \
+		--uuid $(shlibuuid) --in $< --out $@

open-vela/external_optee_optee_os

 struct shdr
  img_type:   3 (SHDR_SUBKEY)
  img_size:   320 bytes
  algo:       0x70414930 (TEE_ALG_RSASSA_PKCS1_PSS_MGF1_SHA256)
  hash_size:  32 bytes
  sig_size:   256 bytes
  hash:       f573f329fe77be686ce71647909c4ea35b5e1cd7de86369bd7d9fca31f6a4d65
 struct shdr_subkey
  hash:       233a6dcf1a2cf69e50cde8e20c4129157da707c76fa86ce12ee31037edef02d7
  uuid:       1a5948c5-1aa0-518c-86f4-be6f6a057b16
  img_type:   1 (SHDR_BOOTSTRAP_TA)
  img_size:   84576 bytes
  hash:       ea31ac7dc2cc06a9dc2853cd791dd00f784b5edc062ecfa274deeb66589b4ca5
 struct shdr_bootstrap_ta
  uuid:       5c206987-16a3-59cc-ab0f-64b9cfc9e758
 TA offset:  1712 (0x6b0) bytes
 TA size:    84576 (0x14a60) bytes
Acked-by: Jerome Forissier <jerome.forissier@linaro.org>
Tested-by: Jerome Forissier <jerome.forissier@linaro.org> (vexpress-qemu_virt)
Acked-by: Etienne Carriere <etienne.carriere@linaro.org>

core/crypto/signed_hdr.c
@@ -1,15 +1,18 @@
 // SPDX-License-Identifier: BSD-2-Clause
 /*
- * Copyright (c) 2015-2017, Linaro Limited
+ * Copyright (c) 2015-2022, Linaro Limited
  */
 
 #include <crypto/crypto.h>
+#include <kernel/panic.h>
+#include <mempool.h>
 #include <signed_hdr.h>
 #include <stdlib.h>
 #include <string.h>
 #include <ta_pub_key.h>
 #include <tee_api_types.h>
 #include <tee/tee_cryp_utl.h>
+#include <tee/uuid.h>
 #include <utee_defines.h>
 #include <util.h>
 
@@ -85,5 +88,268 @@ out:
 	crypto_acipher_free_rsa_public_key(&key);
 	if (res)
 		return TEE_ERROR_SECURITY;
+	return TEE_SUCCESS;
+}
+
+static const struct shdr_subkey_attr *
+find_attr(const struct shdr_subkey *subkey, uint32_t id)
+{
+	size_t n = 0;
+
+	for (n = 0; n < subkey->attr_count; n++)
+		if (subkey->attrs[n].id == id)
+			return subkey->attrs + n;
+
+	return NULL;
+}
+
+static TEE_Result load_rsa_key(const struct shdr_subkey *subkey,
+			       struct rsa_public_key **key_pp)
+{
+	const uint8_t *base = (const uint8_t *)subkey;
+	const struct shdr_subkey_attr *pub_exp = NULL;
+	const struct shdr_subkey_attr *modulus = NULL;
+	struct rsa_public_key *key = NULL;
+	TEE_Result res = TEE_SUCCESS;
+
+	pub_exp = find_attr(subkey, TEE_ATTR_RSA_PUBLIC_EXPONENT);
+	if (!pub_exp)
+		return TEE_ERROR_SECURITY;
+	modulus = find_attr(subkey, TEE_ATTR_RSA_MODULUS);
+	if (!modulus)
+		return TEE_ERROR_SECURITY;
+
+	key = calloc(1, sizeof(*key));
+	if (!key)
+		return TEE_ERROR_OUT_OF_MEMORY;
+	res = crypto_acipher_alloc_rsa_public_key(key, modulus->size * 8);
+	if (res)
+		goto err_key;
+
+	res = crypto_bignum_bin2bn(base + pub_exp->offs, pub_exp->size, key->e);
+	if (res)
+		goto err;
+	res = crypto_bignum_bin2bn(base + modulus->offs, modulus->size, key->n);
+	if (res)
+		goto err;
+
+	*key_pp = key;
+	return TEE_SUCCESS;
+err:
+	crypto_acipher_free_rsa_public_key(key);
+err_key:
+	free(key);
+	return TEE_ERROR_SECURITY;
+}
+
+static TEE_Result check_attrs(const struct shdr_subkey *subkey, size_t img_size)
+{
+	const struct shdr_subkey_attr *attrs = subkey->attrs;
+	size_t end = 0;
+	size_t n = 0;
+
+	if (MUL_OVERFLOW(subkey->attr_count, sizeof(*attrs), &end) ||
+	    ADD_OVERFLOW(end, sizeof(*subkey), &end) ||
+	    end > img_size)
+		return TEE_ERROR_SECURITY;
+
+	for (n = 0; n < subkey->attr_count; n++)
+		if (ADD_OVERFLOW(attrs[n].offs, attrs[n].size, &end) ||
+		    end > img_size)
+			return TEE_ERROR_SECURITY;
+
+	return TEE_SUCCESS;
+}
+
+static TEE_Result calc_next_uuid(uint8_t uuid[sizeof(TEE_UUID)],
+				 const uint8_t my_uuid[sizeof(TEE_UUID)],
+				 const void *ns_name, size_t name_size)
+{
+	TEE_Result res = TEE_ERROR_SECURITY;
+	void *ctx = NULL;
+	struct {
+		uint8_t digest[TEE_SHA1_HASH_SIZE];
+		TEE_UUID uuid;
+		char name_str[];
+	} *tmp = NULL;
+
+	if (!name_size) {
+		memcpy(uuid, my_uuid, sizeof(TEE_UUID));
+		return TEE_SUCCESS;
+	}
+
+	/*
+	 * RFC 4122 requires a SHA-1 digest for UUID v5. Use SHA-512
+	 * instead for better collision resistance.
+	 */
+	if (crypto_hash_alloc_ctx(&ctx, TEE_ALG_SHA512))
+		return TEE_ERROR_SECURITY;
+
+	tmp = mempool_alloc(mempool_default, sizeof(*tmp) + name_size);
+	if (!tmp)
+		goto out_ctx;
+	memcpy(tmp->name_str, ns_name, name_size);
+
+	if (crypto_hash_init(ctx) ||
+	    crypto_hash_update(ctx, my_uuid, sizeof(TEE_UUID)) ||
+	    crypto_hash_update(ctx, (const void *)tmp->name_str,
+			       strnlen(tmp->name_str, name_size)) ||
+	    crypto_hash_final(ctx, tmp->digest, sizeof(tmp->digest)))
+		goto out_mempool;
+
+	tee_uuid_from_octets(&tmp->uuid, tmp->digest);
+	/*
+	 * Set the four most significant bits (bits 12 through 15) of the
+	 * time_hi_and_version field to 5.
+	 */
+	tmp->uuid.timeHiAndVersion &= ~SHIFT_U32(0xf, 12);
+	tmp->uuid.timeHiAndVersion |= SHIFT_U32(5, 12);
+	/*
+	 * Set the two most significant bits (bits 6 and 7) of the
+	 * clock_seq_hi_and_reserved to zero and one, respectively.
+	 */
+	tmp->uuid.clockSeqAndNode[0] &= ~BIT(6);
+	tmp->uuid.clockSeqAndNode[0] |= BIT(7);
+
+	tee_uuid_to_octets(uuid, &tmp->uuid);
+	res = TEE_SUCCESS;
+
+out_mempool:
+	mempool_free(mempool_default, tmp);
+out_ctx:
+	crypto_hash_free_ctx(ctx);
+
+	return res;
+}
+
+TEE_Result shdr_load_pub_key(const struct shdr *shdr, size_t offs,
+			     const uint8_t *ns_img, size_t ns_img_size,
+			     const uint8_t next_uuid[sizeof(TEE_UUID)],
+			     uint32_t max_depth, struct shdr_pub_key *key)
+{
+	struct shdr_subkey *subkey = NULL;
+	TEE_Result res = TEE_SUCCESS;
+	void *digest = NULL;
+	uint8_t *img = NULL;
+	void *ctx = NULL;
+	size_t end = 0;
+
+	if (shdr->img_type != SHDR_SUBKEY)
+		return TEE_ERROR_SECURITY;
+
+	if (shdr->img_size < sizeof(*subkey))
+		return TEE_ERROR_SECURITY;
+
+	if (ADD_OVERFLOW(shdr->img_size, offs, &end) || end > ns_img_size)
+		return TEE_ERROR_SECURITY;
+
+	img = mempool_alloc(mempool_default, shdr->img_size + shdr->hash_size);
+	if (!img)
+		return TEE_ERROR_OUT_OF_MEMORY;
+	memcpy(img + shdr->hash_size, ns_img + offs, shdr->img_size);
+	subkey = (void *)(img + shdr->hash_size);
+	digest = img;
+
+	if (crypto_hash_alloc_ctx(&ctx, TEE_DIGEST_HASH_TO_ALGO(shdr->algo))) {
+		res = TEE_ERROR_SECURITY;
+		goto out_mempool;
+	}
+
+	if (crypto_hash_init(ctx) ||
+	    crypto_hash_update(ctx, (const void *)shdr, sizeof(*shdr)) ||
+	    crypto_hash_update(ctx, (const void *)subkey, shdr->img_size) ||
+	    crypto_hash_final(ctx, digest, shdr->hash_size) ||
+	    memcmp(digest, SHDR_GET_HASH(shdr), shdr->hash_size)) {
+		res = TEE_ERROR_SECURITY;
+		goto out_ctx;
+	}
+
+	res = check_attrs(subkey, shdr->img_size);
+	if (res)
+		goto out_ctx;
+
+	if (subkey->max_depth >= max_depth) {
+		res = TEE_ERROR_SECURITY;
+		goto out_ctx;
+	}
+	if (next_uuid && memcmp(next_uuid, subkey->uuid, sizeof(TEE_UUID))) {
+		res = TEE_ERROR_SECURITY;
+		goto out_ctx;
+	}
+
+	key->max_depth = subkey->max_depth;
+	key->name_size = subkey->name_size;
+	memcpy(key->uuid, subkey->uuid, sizeof(TEE_UUID));
+	if (ADD_OVERFLOW(key->name_size, offs + shdr->img_size, &end) ||
+	    end > ns_img_size) {
+		res = TEE_ERROR_SECURITY;
+		goto out_ctx;
+	}
+	res = calc_next_uuid(key->next_uuid, key->uuid,
+			     ns_img + offs + shdr->img_size, key->name_size);
+	if (res)
+		goto out_ctx;
+
+	key->main_algo = TEE_ALG_GET_MAIN_ALG(subkey->algo);
+	switch (key->main_algo) {
+	case TEE_MAIN_ALGO_RSA:
+		res = load_rsa_key(subkey, &key->pub_key.rsa);
+		break;
+	default:
+		res = TEE_ERROR_SECURITY;
+		break;
+	}
+
+out_ctx:
+	crypto_hash_free_ctx(ctx);
+out_mempool:
+	mempool_free(mempool_default, img);
+	return res;
+}
+
+void shdr_free_pub_key(struct shdr_pub_key *key)
+{
+	if (key) {
+		switch (key->main_algo) {
+		case TEE_MAIN_ALGO_RSA:
+			crypto_acipher_free_rsa_public_key(key->pub_key.rsa);
+			free(key->pub_key.rsa);
+			break;
+		default:
+			panic();
+		}
+	}
+}
+
+TEE_Result shdr_verify_signature2(struct shdr_pub_key *key,
+				  const struct shdr *shdr)
+{
+	size_t hash_size = 0;
+
+	if (shdr->magic != SHDR_MAGIC)
+		return TEE_ERROR_SECURITY;
+
+	if (TEE_ALG_GET_MAIN_ALG(shdr->algo) != key->main_algo)
+		return TEE_ERROR_SECURITY;
+
+	if (tee_alg_get_digest_size(TEE_DIGEST_HASH_TO_ALGO(shdr->algo),
+				    &hash_size) ||
+	    hash_size != shdr->hash_size)
+		return TEE_ERROR_SECURITY;
+
+	switch (key->main_algo) {
+	case TEE_MAIN_ALGO_RSA:
+		if (crypto_acipher_rsassa_verify(shdr->algo, key->pub_key.rsa,
+						 shdr->hash_size,
+						 SHDR_GET_HASH(shdr),
+						 shdr->hash_size,
+						 SHDR_GET_SIG(shdr),
+						 shdr->sig_size))
+			return TEE_ERROR_SECURITY;
+		break;
+	default:
+		panic();
+	}
+
 	return TEE_SUCCESS;
 }

ta/arch/arm/link_shlib.mk
@@ -48,7 +48,7 @@ $(link-out-dir)/$(shlibuuid).elf: $(link-out-dir)/$(shlibname).so
 	$(q)ln -sf $(<F) $@
 
 $(link-out-dir)/$(shlibuuid).ta: $(link-out-dir)/$(shlibname).stripped.so \
-				$(TA_SIGN_KEY)
+				$(TA_SIGN_KEY) $(TA_SUBKEY_DEPS)
 	@$(cmd-echo-silent) '  SIGN    $@'
-	$(q)$(PYTHON3) $(SIGN) --key $(TA_SIGN_KEY) --uuid $(shlibuuid) \
-		--in $< --out $@
+	$(q)$(PYTHON3) $(SIGN) --key $(TA_SIGN_KEY) $(TA_SUBKEY_ARGS) \
+		--uuid $(shlibuuid) --in $< --out $@