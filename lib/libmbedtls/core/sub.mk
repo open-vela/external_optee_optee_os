OP-TEE/optee_os

libmbedtls: support mbedTLS in kernel mode
Initial step of mbedtls cryptos integration.
Directory created and interface file is drafted.
All function interfaces are set to "not supported".
The mbedtls can be selected by specifying build flags
"CFG_CRYPTOLIB_NAME=mbedtls" and "CFG_CRYPTOLIB_DIR=lib/libmbedtls"

Signed-off-by: Edison Ai <edison.ai@arm.com>
Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>

lib/libmbedtls/core/sub.mk
@@ -1,0 +1,3 @@
+cflags-lib-$(CFG_CRYPTO_SIZE_OPTIMIZATION) += -Os
+
+srcs-y += tee_lmd_provider.c

open-vela/external_optee_optee_os

Reviewed-by: Etienne Carriere <etienne.carriere@linaro.org>
Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>

lib/libmbedtls/core/sub.mk
@@ -1,0 +1,3 @@
+cflags-lib-$(CFG_CRYPTO_SIZE_OPTIMIZATION) += -Os
+
+srcs-y += stubbed.c