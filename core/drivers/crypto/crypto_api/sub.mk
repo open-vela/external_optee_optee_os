OP-TEE/optee_os

core: driver: generic resources for crypto device driver
Add a generic cryptographic driver interface connecting
TEE Crypto generic APIs to HW driver interface

The Generic Crypto Driver interface in the core/driver/crypto/crypto_api
is implemented to be able to use a HW driver.

Signed-off-by: Cedric Neveux <cedric.neveux@nxp.com>

core/drivers/crypto/crypto_api/sub.mk
@@ -1,0 +1,3 @@
+srcs-y += drvcrypt_init.c
+
+subdirs-$(CFG_CRYPTO_DRV_HASH) += hash

core/include/crypto/crypto_impl.h
@@ -27,6 +27,18 @@ struct crypto_hash_ops {
 			   struct crypto_hash_ctx *src_ctx);
 };
 
+#ifdef CFG_CRYPTO_DRIVER
+/* Cryptographic HASH driver context allocation */
+TEE_Result drvcrypt_hash_alloc_ctx(struct crypto_hash_ctx **ctx, uint32_t algo);
+#else
+static inline TEE_Result
+drvcrypt_hash_alloc_ctx(struct crypto_hash_ctx **ctx __unused,
+			uint32_t algo __unused)
+{
+	return TEE_ERROR_NOT_IMPLEMENTED;
+}
+#endif
+
 #define CRYPTO_ALLOC_CTX_NOT_IMPLEMENTED(name, type) \
 	static inline TEE_Result \
 	crypto_##name##_alloc_ctx(struct crypto_##type##_ctx **ctx __unused) \

open-vela/external_optee_optee_os

Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>
Reviewed-by: Etienne Carriere <etienne.carriere@linaro.org>
Acked-by: Jerome Forissier <jerome@forissier.org>

core/drivers/crypto/crypto_api/sub.mk
@@ -1,0 +1,3 @@
+srcs-y += drvcrypt.c
+
+subdirs-$(CFG_CRYPTO_DRV_HASH) += hash

core/include/crypto/crypto_impl.h
@@ -232,4 +232,15 @@ struct crypto_authenc_ops {
 
 TEE_Result crypto_aes_ccm_alloc_ctx(struct crypto_authenc_ctx **ctx);
 TEE_Result crypto_aes_gcm_alloc_ctx(struct crypto_authenc_ctx **ctx);
+
+#ifdef CFG_CRYPTO_DRV_HASH
+TEE_Result drvcrypt_hash_alloc_ctx(struct crypto_hash_ctx **ctx, uint32_t algo);
+#else
+static inline TEE_Result
+drvcrypt_hash_alloc_ctx(struct crypto_hash_ctx **ctx __unused,
+			uint32_t algo __unused)
+{
+	return TEE_ERROR_NOT_IMPLEMENTED;
+}
+#endif
 #endif /*__CRYPTO_CRYPTO_IMPL_H*/